e32a021478fba3891661764b615bc4c6c1e63ec7
khongloan <khongloan1999@gmail.com>
2020-07-03 00:37:37 +0700

    Working

--- app.py
+++ app.py
@@ -10,8 +10,24 @@
 # if __name__ == '__main__':
 #     app.run(debug=True,host='0.0.0.0')
 
-
-
+from featureextraction import extract_features
+import nltk
+#nltk.download('punkt')
+import nltk.data
+import pyaudio
+import wave
+import pickle
+import numpy as np
+from sklearn import mixture
+import sys
+from pynput import keyboard
+import sounddevice as sd
+import soundfile as sf
+import time
+import os
+from scipy.io.wavfile import read
+from featureextraction import extract_features
+import warnings
 from flask import Flask, render_template, request
 import mysql.connector
 import os
@@ -26,16 +42,17 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="pageone1Q",
-  database="speakerrecognition"
+  database="speakerrecognition",
+  auth_plugin='mysql_native_password'
 )
 
 mycursor = mydb.cursor()
 
-# mycursor.execute("CREATE TABLE host (host_id INT AUTO_INCREMENT PRIMARY KEY, host_name VARCHAR(255))")
+mycursor.execute("CREATE TABLE host (host_id INT AUTO_INCREMENT PRIMARY KEY, host_name VARCHAR(255))")
 
-# mycursor.execute("CREATE TABLE user (client_id VARCHAR(255) PRIMARY KEY,userid INT, name VARCHAR(255), is_attending BOOLEAN, is_host BOOLEAN, train_folder VARCHAR(255), test_folder VARCHAR(255) )")
+mycursor.execute("CREATE TABLE user (client_id VARCHAR(255) PRIMARY KEY,userid INT, name VARCHAR(255), is_attending BOOLEAN, is_host BOOLEAN, train_folder VARCHAR(255), test_folder VARCHAR(255) )")
 
-# mycursor.execute("CREATE TABLE host_user (host_user_id  INT AUTO_INCREMENT PRIMARY KEY, host_id INT, client_id VARCHAR(255) , FOREIGN KEY (host_id) REFERENCES host (host_id) ON DELETE CASCADE  ON UPDATE CASCADE, FOREIGN KEY (client_id) REFERENCES user (client_id) ON DELETE CASCADE  ON UPDATE CASCADE)")
+mycursor.execute("CREATE TABLE host_user (host_user_id  INT AUTO_INCREMENT PRIMARY KEY, host_id INT, client_id VARCHAR(255) , FOREIGN KEY (host_id) REFERENCES host (host_id) ON DELETE CASCADE  ON UPDATE CASCADE, FOREIGN KEY (client_id) REFERENCES user (client_id) ON DELETE CASCADE  ON UPDATE CASCADE,is_attending BOOLEAN)")
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -52,7 +69,7 @@ def createTeacher():
   print("USERID"+ str(userid))
   name = content['name']
   client_id = str(userid)+'_'+name
-  is_attending = False
+  
   is_host = True
   train_folder = 'local/bin/usr/voice_recog/train_folder/'+str(client_id)
   if not os.path.exists(train_folder):
@@ -62,7 +79,7 @@ def createTeacher():
     os.makedirs(test_folder)
 
   cur = mysql.connection.cursor()
-  cur.execute("INSERT INTO user(client_id, userid, name, is_attending, is_host, train_folder, test_folder) VALUES (%s, %s,%s, %s,%s, %s,%s)", (client_id, userid, name, is_attending, is_host, train_folder, test_folder))
+  cur.execute("INSERT INTO user(client_id, userid, name,  is_host, train_folder, test_folder) VALUES (%s, %s,%s, %s, %s,%s)", (client_id, userid, name, is_host, train_folder, test_folder))
   mysql.connection.commit()
 
   cur.execute( "SELECT * FROM user WHERE client_id = %s", [client_id] )
@@ -79,7 +96,6 @@ def createStudent():
   print("USERID"+ str(userid))
   name = content['name']
   client_id = str(userid)+'_'+name
-  is_attending = False
   is_host = False
   train_folder = 'local/bin/usr/voice_recog/train_folder/'+str(client_id)
   if not os.path.exists(train_folder):
@@ -89,7 +105,7 @@ def createStudent():
     os.makedirs(test_folder)
 
   cur = mysql.connection.cursor()
-  cur.execute("INSERT INTO user(client_id, userid, name, is_attending, is_host, train_folder, test_folder) VALUES (%s, %s,%s, %s,%s, %s,%s)", (client_id, userid, name, is_attending, is_host, train_folder, test_folder))
+  cur.execute("INSERT INTO user(client_id, userid, name, is_host, train_folder, test_folder) VALUES (%s, %s, %s,%s, %s,%s)", (client_id, userid, name, is_host, train_folder, test_folder))
   mysql.connection.commit()
   cur.execute( "SELECT * FROM user WHERE client_id = %s", [client_id] )
   user = cur.fetchone()
@@ -170,11 +186,11 @@ def userInRoom():
   host_id = request.args.get('host_id')
   cur = mysql.connection.cursor()
 
-  cur.execute( "SELECT client_id FROM host_user WHERE host_id = %s", [host_id] )
+  cur.execute( "SELECT client_id FROM host_user WHERE host_id = %s  AND is_attending = 1", [host_id] )
   data = cur.fetchall()
   user_list = []
   for row in data:
-    cur.execute( "SELECT * FROM user WHERE client_id = %s AND is_attending = 1", [row[0]] )
+    cur.execute( "SELECT * FROM user WHERE client_id = %s", [row[0]] )
     user = cur.fetchone()
     if user is not None:
       user_list.append(user)
@@ -223,8 +239,117 @@ def upload_test_file():
   testFolder = user[0]
   f = request.files['testfile']
   f.save(os.path.join(testFolder, secure_filename(f.filename)))
-  
+  # train_file = "newmodeltest.txt" 
+  # fileopen = open(train_file, 'w+')
+  # fileopen.write(os.path.join(testFolder, secure_filename(f.filename)))
   return Response('file uploaded successfully', status=201, mimetype='application/json')
+
+@app.route('/buildModel', methods = [ 'GET'])
+def buildModel():
+  client_id = request.args.get('client_id')
+  cur = mysql.connection.cursor()
+  cur.execute( "SELECT train_folder FROM user WHERE client_id = %s", [client_id] )
+  user = cur.fetchone()
+  trainFolder = user[0]
+  print("Train folder")
+  print(trainFolder)
+  train_file = "newmodeltrain.txt" 
+  fileopen = open(train_file, 'w+')
+  for filename in os.listdir(trainFolder):
+    #if filename.endswith(".wav"):
+    print(os.path.join(trainFolder, filename))
+    fileopen.write(str(trainFolder +"/"+filename+"\n"))
+  fileopen.close()
+
+
+    
+  
+  dest = "Speakers_models/"
+  file_paths = open(train_file,'r')
+  features = np.asarray(())
+  for path in file_paths: 
+    path = path.strip()   
+    print (path)
+    
+    sr,audio = read(path)
+    
+    vector   = extract_features(audio,sr)
+    
+    if features.size == 0:
+      features = vector
+    else:
+      features = np.vstack((features, vector))
+  
+  print("Feature")
+  print(features)
+  gmm = mixture.GaussianMixture(n_components = 9, max_iter = 200, covariance_type='diag',n_init = 3)
+  gmm.fit(features)
+  
+  # dumping the trained gaussian model
+  picklefile = client_id+".gmm"
+  #pickle.dump(gmm,open(dest + picklefile,'wb'))
+  with open(dest + picklefile,'wb') as file:
+      pickle.dump(gmm, file)
+  print ('+ modeling completed for speaker:',picklefile," with data point = ",features.shape)
+  features = np.asarray(())
+    
+
+
+
+
+
+  return Response('Build model successfully', status=201, mimetype='application/json')
+
+@app.route('/rollRoom', methods = [ 'POST'])
+def rollRoom():
+  host_id = request.args.get('host_id')
+  client_id = request.args.get('client_id')
+  cur = mysql.connection.cursor()
+  cur.execute( "SELECT test_folder FROM user WHERE client_id = %s", [client_id] )
+  user = cur.fetchone()
+  testFolder = user[0]
+  
+  modelpath = "Speakers_models/"
+  gmm_files = [os.path.join(modelpath,fname) for fname in  os.listdir(modelpath) if fname.endswith('.gmm')]
+
+  models = [pickle.load(open(fname,'rb')) for fname in gmm_files]
+  speakers = [fname.split("/")[-1].split(".gmm")[0] for fname in gmm_files]
+
+  path = ""
+  for filename in os.listdir(testFolder):
+    if filename.endswith(".wav"):
+      print(os.path.join(testFolder, filename))
+      path = os.path.join(testFolder, filename)
+      break
+
+  print("PATH")
+  print(path)
+  sr,audio = read( path)
+  vector   = extract_features(audio,sr)
+
+  log_likelihood = np.zeros(len(models)) 
+
+  for i in range(len(models)):
+    gmm = models[i]  #checking with each model one by one
+    scores = np.array(gmm.score(vector))
+    log_likelihood[i] = scores.sum()
+
+  winner = np.argmax(log_likelihood)
+  print ("\tdetected as - ", speakers[winner])
+
+
+  if speakers[winner] == client_id:
+    mycursor = mydb.cursor()
+
+    sql = "UPDATE host_user SET is_attending = %s WHERE client_id = %s AND host_id = %s"
+    val = (True, client_id, host_id)
+
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+    return Response(speakers[winner], status=200, mimetype='application/json')
+
+  return Response(speakers[winner], status=400, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
--- modeltraining.py
+++ modeltraining.py
@@ -5,7 +5,6 @@ from scipy.io.wavfile import read
 from sklearn import mixture
 
 from featureextraction import extract_features
-#from speakerfeatures import extract_features
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -23,7 +22,6 @@ train_file = "trainingDataPath.txt"
 file_paths = open(train_file,'r')
 
 count = 1
-# Extracting features for each speaker (5 files per speakers)
 features = np.asarray(())
 for path in file_paths:    
     path = path.strip()   
@@ -32,15 +30,13 @@ for path in file_paths:
     # read the audio
     sr,audio = read(source + path)
     
-    # extract 40 dimensional MFCC & delta MFCC features
     vector   = extract_features(audio,sr)
     
     if features.size == 0:
         features = vector
     else:
         features = np.vstack((features, vector))
-    # when features of 5 files of speaker are concatenated, then do model training
-	# -> if count == 5: --> edited below
+  
     if count == 15:    
         gmm = mixture.GaussianMixture(n_components = 16, max_iter = 200, covariance_type='diag',n_init = 3)
         gmm.fit(features)
