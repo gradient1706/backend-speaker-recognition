67a0f7fbf3c8b276796d7dda91216f9a2a204996
Le Toan <toanlk@sapo.vn>
2020-07-03 12:41:59 +0700

    fixbug

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-database = MySQLdb.connect(host = "localhost", user = "root", passwd = "pageone1Q", db = "speakerrecognition", cursorclass=MySQLdb.cursors.DictCursor)
-c = database.cursor()
+# database = MySQLdb.connect(host = "localhost", user = "root", passwd = "pageone1Q", db = "speakerrecognition", cursorclass=MySQLdb.cursors.DictCursor)
+# c = database.cursor()
 
 # mycursor.execute("CREATE TABLE host (host_id INT AUTO_INCREMENT PRIMARY KEY, host_name VARCHAR(255))")
 
@@ -47,10 +47,10 @@ c = database.cursor()
 
 # mycursor.execute("CREATE TABLE host_user (host_user_id  INT AUTO_INCREMENT PRIMARY KEY, host_id INT, client_id VARCHAR(255) , FOREIGN KEY (host_id) REFERENCES host (host_id) ON DELETE CASCADE  ON UPDATE CASCADE, FOREIGN KEY (client_id) REFERENCES user (client_id) ON DELETE CASCADE  ON UPDATE CASCADE,is_attending BOOLEAN)")
 
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'pageone1Q'
-# app.config['MYSQL_DB'] = 'speakerrecognition'
+app.config['MYSQL_HOST'] = 'us-cdbr-east-02.cleardb.com'
+app.config['MYSQL_USER'] = 'bc230c0c0a241d'
+app.config['MYSQL_PASSWORD'] = '456abe01'
+app.config['MYSQL_DB'] = 'heroku_768243426c2ce6a'
 
 # mysql = MySQL(app)
 
